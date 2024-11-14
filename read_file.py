20e14cb77767ccb3c0523dd564255e993dfc7839
yzdesk <2013978679@qq.com>
2024-11-14 21:21:19 +0800

    ignore

--- read_file.py
+++ read_file.py
@@ -1,7 +1,6 @@
 # 读取二进制文件
 import time
-
-with open('file_system/disk/MyDisk.dat', 'rb') as f:
+with open('Disk\MyDisk.dat', 'rb') as f:
     start_time = time.time()
     data = f.read()
     end_time = time.time()
