913595168b436cb1d8b65248c381517473193136
yzdesk <2013978679@qq.com>
2024-11-19 13:40:34 +0800

    1119

--- read_file.py
+++ read_file.py
@@ -1,9 +1,15 @@
 # 读取二进制文件
 import time
-with open('Disk\MyDisk.dat', 'rb') as f:
+with open('Disk\\MyDisk.dat', 'rb') as f:
     start_time = time.time()
     data = f.read()
     end_time = time.time()
     print(data)  
 
-print('读取文件耗时：', end_time - start_time)
+print('读取文件耗时：', end_time - start_time)std::string welcome1 = 
+"    _______ __        _____            __                \n"
+"   / ____(_/ ___     / ___/__  _______/ /____  ____ ___  \n"
+"  / /_  / / / _ \\    \\__ \\/ / / / ___/ __/ _ \\/ __ `__ \\ \n"
+" / __/ / / /  __/   ___/ / /_/ (__  / /_/  __/ / / / / / \n"
+"/_/   /_/_/\\___/   /____/\\__, /____/\\__/\\___/_/ /_/ /_/  \n"
+"                        /____/                           \n";
